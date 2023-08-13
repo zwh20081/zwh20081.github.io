bc1beccf55be1f02c3072dec792fc61ecd567c86
zwh20081 <zwh20081@qq.com>
2023-08-14 00:13:16 +0800

    my up

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export default {
   // 标题
-  title: '遥远的理想乡 - TsukiSeele\'s Blog',
+  title: 'Tired but going on-Blog of Zwh20081',
   // 音乐接口
   musicAPI: 'https://api.hlo.li/music/playlist/detail?id=7490559834',
   // 一言接口（可选，填写后会显示在subtitle)
@@ -8,7 +8,7 @@ export default {
   // 静态资源位置（可选）
   static: 'https://cdn.jsdelivr.net/gh/tsukiseele/awsl.re/static',
   // base64编码的Github token
-  token: 'Z2hwX2UzbXo0eDFzMW1JeTlYYjJBYzZoc3lvMVA5eFpLaTN0ZnoxbA==',
+  token: 'Z2hwX2dJSzc0VTcxVnJJMHZqd2Uzc3ZEMVpEM2QzaHgxSzBKNFNtMA==',
   // Github OAuth口令，參見https://vssue.js.org/guide/github.html#config-and-start-your-vssue
   oAuth: {
     // Pre
@@ -18,26 +18,26 @@ export default {
   // 博客配置，考慮到GFW，建議不要直接在Issue插入圖片，請使用外鏈以 提升速度和穩定性
   blog: {
     // Github 用户名
-    username: 'tsukiseele',
+    username: 'zwh20081',
     // Github 存储文章的仓库名
-    repository: 'archives',
+    repository: 'work of a failure',
     // 每頁顯示的文章數
     paginationSize: 8,
   },
   // 评论配置，雖然評論可以和文章儲存在同一倉庫，但是考慮到需要過濾且影響可讀性，所以請不要這麽做
   vssue: {
-    owner: 'tsukiseele',
+    owner: 'zwh20081',
     repo: 'comment',
   },
   // 导航
   nav: {
     // title: 'さとうしお',
-    title: '遥远的理想乡',
-    subtitle: "TsukiSeele's Blog",
-    owner: 'TsukiSeele',
-    introduction: 'しおちゃんがいる場所が、私のハッピーシュガーライフ',
-    introductionFrom: 'ーー松坂さとう',
-    decorateText: 'SWEET',
+    title: 'Dream home, warm house',
+    subtitle: "zwh20081's Blog",
+    owner: 'zwh20081',
+    introduction: 'Pointless intro, Useless',
+    introductionFrom: '--me',
+    decorateText: 'ALIVE',
     drawer: false,
     drawerBannerBackground: 'https://cdn.jsdelivr.net/gh/tsukiseele/ImageHosting/upload/826f66f94e3ebf1f62cff7c9109bb118.jpeg',
     nav: [
@@ -48,9 +48,9 @@ export default {
       { name: 'About', icon: 'about', to: '/about' },
     ],
     links: [
-      { icon: 'github', to: 'https://github.com/tsukiseele' },
-      { icon: 'twitter', to: 'https://twitter.com/tsukiseele' },
-      { icon: 'mail', to: 'mailto:tsukiseele@gmail.com' },
+      { icon: 'github', to: 'https://github.com/zwh20081' },
+      { icon: 'twitter', to: 'https://twitter.com/zwh20081' },
+      { icon: 'mail', to: 'mailto:zwh20081@qq.com' },
     ],
   },
   // 页面元数据
@@ -65,7 +65,7 @@ export default {
     {
       hid: 'description',
       name: 'description',
-      content: '这里我的个人网站さとうしお、分享来自二次元的奇迹，请多关照',
+      content: '我的blog，为了存在而存在着',
     },
   ],
   backgroundTheme: 'madras',
